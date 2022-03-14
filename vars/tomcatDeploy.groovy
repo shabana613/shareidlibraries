c6764667b047e861fc15769350194af2711a8e2c
shabana613 <72884508+shabana613@users.noreply.github.com>
2022-03-14 15:49:00 +0530

    Create tomcatDeploy.groovy

---
+++ vars/tomcatDeploy.groovy
@@ -0,0 +1,7 @@
+def call(ips,user,credId){
+  sshagent([credId]) { 
+  sh "scp -o StrictHostKeyChecking=no target/*.war ${user}@${ips}:/opt/tomcat8/webapps/app.war"
+  sh "ssh ${user}@${ips} /opt/tomcat8/bin/shutdown.sh"
+  sh "ssh ${user}@${ips} /opt/tomcat8/bin/startup.sh"
+  }
+}
