d82a908dc5f7b414f07e083e2a861add5f1deda2
shabana613 <72884508+shabana613@users.noreply.github.com>
2022-03-14 16:11:05 +0530

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -2,9 +2,9 @@ def call(ips,user,credId){
   sshagent([credId]) { 
  //   ips.each {
  //     echo "Deploying on ${it}"
-      sh "scp -o StrictHostKeyChecking=no target/*.war ${user}@${it}:/opt/tomcat8/webapps/app.war"
-      sh "ssh ${user}@${it} /opt/tomcat8/bin/shutdown.sh"
-      sh "ssh ${user}@${it} /opt/tomcat8/bin/startup.sh"
+      sh "scp -o StrictHostKeyChecking=no target/*.war ${user}@${ips}:/opt/tomcat8/webapps/app.war"
+      sh "ssh ${user}@${ips} /opt/tomcat8/bin/shutdown.sh"
+      sh "ssh ${user}@${ips} /opt/tomcat8/bin/startup.sh"
     }
   } 
 //}
