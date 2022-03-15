48ad9985367fa78525e7da364d23e7facaa2f1ab
shabana613 <72884508+shabana613@users.noreply.github.com>
2022-03-15 11:43:50 +0530

    Update calculator.groovy

--- vars/calculator.groovy
+++ vars/calculator.groovy
@@ -1,9 +1,9 @@
 def add(x,y){
-  print "sum of $(x) and $(y) is $(x+y)"
+  print "Sum of ${x} and ${y} is ${x+y}"
 }
 def sub(x,y){
-  print "subtraction of $(x) and $(y) is $(x-y)"
+  print "substraction of ${x} and ${y} is ${x-y}"
 }
 def mul(x,y){
-  print "multplication of $(x) and $(y) is $(x*y)"
+  print "Multiplication of ${x} and ${y} is ${x*y}"
 }
